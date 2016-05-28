2fd6b9426597ad0f0a52e77b1d203d2f22c7708d
kancho <kanchokanev7@gmail.com>
2016-05-28 20:48:59 +0300

    added product details page

--- KaliStore.WebApp/src/area/products/all-products/all-products.js
+++ KaliStore.WebApp/src/area/products/all-products/all-products.js
@@ -1,20 +1,14 @@
-import {AddProductToCartEvent} from 'events';
-import {EventAggregator} from 'aurelia-event-aggregator';
 import {inject} from 'aurelia-framework';
 import {ProductsRepository} from 'repository';
 
-@inject(EventAggregator, ProductsRepository)
+@inject(ProductsRepository)
 export class AllProducts {
   products = [];
   ratings = [5,4,3,2,1];
 
-  constructor(eventAggregator, productsRepository) {
-    this.eventAggregator = eventAggregator;
+  constructor(productsRepository) {
     this.productsRepository = productsRepository;
     this.products = this.productsRepository.getAll();
   }
 
-  addToCart(product) {
-    this.eventAggregator.publish(new AddProductToCartEvent(product, 1));
-  }
 }
--- KaliStore.WebApp/src/area/products/product/product.js
+++ KaliStore.WebApp/src/area/products/product/product.js
@@ -1,9 +1,21 @@
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {inject} from 'aurelia-framework';
+import {ProductsRepository} from 'repository';
+import {AddProductToCartEvent} from 'events';
+
+@inject(EventAggregator, ProductsRepository)
 export class Product {
+  constructor(eventAggregator, productsRepository) {
+    this.eventAggregator = eventAggregator;
+    this.productsRepository = productsRepository;
+  }
+  
   activate(routeParams) {
-    this.productId = routeParams.productId;
+    this.product = this.productsRepository.get(parseInt(routeParams.productId));
+    this.product.materials = this.product.materials.map(m => m.name).join(', ')
   }
 
-  addToCart(product) {
-    this.eventAggregator.publish(new AddProductToCartEvent(product, 1));
+  addToCart() {
+    this.eventAggregator.publish(new AddProductToCartEvent(this.product, 1));
   }
 }
