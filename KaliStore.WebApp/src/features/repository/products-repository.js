2c26bf634cc9f436aaef48100c545b963d17afc8
tsvgeorgieva <tsv.georgieva@abv.bg>
2016-12-31 19:38:38 +0200

    products repo refactoring

--- KaliStore.WebApp/src/features/repository/products-repository.js
+++ KaliStore.WebApp/src/features/repository/products-repository.js
@@ -1,15 +1,12 @@
 import {inject} from 'aurelia-framework';
-import moment from 'moment';
 import {localStorageManager} from 'service';
 import {HaikuHttp} from './../service/http-client/haiku-http';
-import {MaterialsRepository} from './materials-repository';
-import {CategoriesRepository} from './categories-repository';
 import {ReviewsRepository} from './reviews-repository';
 import {mappers} from './mappers';
 
 const productsKey = 'products';
 
-@inject(MaterialsRepository, CategoriesRepository, ReviewsRepository, HaikuHttp)
+@inject(ReviewsRepository, HaikuHttp)
 export class ProductsRepository {
   editableProperties = [
     'title',
@@ -23,76 +20,36 @@ export class ProductsRepository {
     'daysToMake'
   ];
 
-  constructor(materialsRepository, categoriesRepository, reviewsRepository, haikuHttp) {
-    this.materialsRepository = materialsRepository;
-    this.categoriesRepository = categoriesRepository;
+  constructor(reviewsRepository, haikuHttp) {
     this.reviewsRepository = reviewsRepository;
     this.http = haikuHttp;
 
-    this.products = this._getAllFromLocalStorage();
-    if (this.products.length === 0) {
-      this._initialize();
-    }
+    this.products = [];
   }
 
   get(id) {
     return this.http.get('catalog/productById', {productId: id})
-      .then(response => {
-        response.product.categories = mappers.objToArray(response.product.categories);
-        response.product.materials = mappers.objToArray(response.product.materials);
-        response.product.price = mappers.amountToPrice(response.product.price);
-        return response.product;
-      })
+      .then(x => this._parseResponse(x)[0])
       .catch(x => console.log(x));
   }
 
   getAll(copy = false) {
     return this.http.get('catalog/allProducts')
-      .then(response => {
-        response.product = mappers.objToArray(response.product);
-        response.product = response.product.map(p => {
-          p.categories = mappers.objToArray(p.categories);
-          p.materials = mappers.objToArray(p.materials);
-          p.price = mappers.amountToPrice(p.price);
-          p.rating = this.reviewsRepository.getRatingForProduct(p.id);
-          return p;
-        });
-
-        return response.product;
-      }).catch(x => console.log(x));
+      .then(x => this._parseResponse(x))
+      .catch(x => console.log(x));
   }
 
   getByQuery(query) {
     const lowerCaseQuery = query.toLocaleLowerCase();
     return this.http.get('catalog/productsByQuery', {query: lowerCaseQuery})
-      .then(response => {
-        response.product = mappers.objToArray(response.product);
-        response.product = response.product.map(p => {
-          p.categories = mappers.objToArray(p.categories);
-          p.materials = mappers.objToArray(p.materials);
-          p.price = mappers.amountToPrice(p.price);
-          p.rating = this.reviewsRepository.getRatingForProduct(p.id);
-          return p;
-        });
-
-        return response.product;
-      }).catch(x => console.log(x));
+      .then(x => this._parseResponse(x))
+      .catch(x => console.log(x));
   }
 
   getByCategory(categoryId) {
     return this.http.get('catalog/productsByCategory', {categoryId: categoryId})
-      .then(response => {
-        response.product = mappers.objToArray(response.product);
-        response.product = response.product.map(p => {
-          p.categories = mappers.objToArray(p.categories);
-          p.materials = mappers.objToArray(p.materials);
-          p.price = mappers.amountToPrice(p.price);
-          p.rating = this.reviewsRepository.getRatingForProduct(p.id);
-          return p;
-        });
-
-        return response.product;
-      }).catch(x => console.log(x));
+      .then(x => this._parseResponse(x))
+      .catch(x => console.log(x));
   }
 
   save(productData) {
@@ -110,26 +67,6 @@ export class ProductsRepository {
     this._saveAllToLocalStorage();
   }
 
-  _initialize() {
-    this.products = initialProducts.map(p => {
-      p.materials = p.materials.map(m => this.materialsRepository.get(m.id));
-      p.category = this.categoriesRepository.get(p.category.id);
-      p.rating = this.reviewsRepository.getRatingForProduct(p.id);
-      return p;
-    });
-    this._saveAllToLocalStorage();
-  }
-
-  _getAllFromLocalStorage() {
-    return (localStorageManager.get(productsKey) || []).map(p => {
-      p.materials = p.materials.map(m => this.materialsRepository.get(m.id));
-      p.category = this.categoriesRepository.get(p.category.id);
-      p.rating = this.reviewsRepository.getRatingForProduct(p.id);
-      p.dateTimeAdded = moment(p.dateTimeAdded);
-      return p;
-    });
-  }
-
   _saveAllToLocalStorage() {
     localStorageManager.save(productsKey, this.products);
   }
@@ -137,222 +74,17 @@ export class ProductsRepository {
   _editProperty(product, productData, property) {
     product[property] = productData[property] || product[property];
   }
-}
 
-const initialProducts = [{
-  id: 1,
-  title: 'Пролетна картичка',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
-  'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  price: {
-    amount: 10,
-    currency: 'BGN'
-  },
-  rating: 4.1,
-  materials: [{
-    id: 1,
-    name: 'Картон'
-  }],
-  size: '20см x 10см',
-  picture: `assets/images/kartichka1.jpg`,
-  category: {
-    id: 1,
-    name: 'Картички'
-  },
-  daysToMake: 2
-}, {
-  id: 2,
-  title: 'Картичка с рози',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
-  'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  price: {
-    amount: 10,
-    currency: 'BGN'
-  },
-  rating: 4.7,
-  materials: [{
-    id: 1,
-    name: 'Картон'
-  }],
-  size: '20см x 10см',
-  picture: `assets/images/kartichka2.jpg`,
-  category: {
-    id: 1,
-    name: 'Картички'
-  },
-  daysToMake: 2
-}, {
-  id: 3,
-  title: 'Коледна картичка с елен',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
-  'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  price: {
-    amount: 10,
-    currency: 'BGN'
-  },
-  rating: 4.6,
-  materials: [{
-    id: 1,
-    name: 'Картон'
-  }, {
-    id: 2,
-    name: 'Брокат'
-  }],
-  size: '20см x 10см',
-  picture: `assets/images/kartichka3.jpg`,
-  category: {
-    id: 1,
-    name: 'Картички'
-  },
-  daysToMake: 2
-}, {
-  id: 4,
-  title: 'Детска торта от картон',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
-  'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  price: {
-    amount: 30,
-    currency: 'BGN'
-  },
-  rating: 4.6,
-  materials: [{
-    id: 1,
-    name: 'Картон'
-  }, {
-    id: 3,
-    name: 'Стикери'
-  }, {
-    id: 4,
-    name: 'Панделка'
-  }],
-  size: '40см x 40см x 20см',
-  picture: `assets/images/torta.jpg`,
-  category: {
-    id: 2,
-    name: 'Торти'
-  },
-  daysToMake: 5
-}, {
-  id: 5,
-  title: 'Детска диадема',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
-  'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  price: {
-    amount: 8,
-    currency: 'BGN'
-  },
-  rating: 4.1,
-  materials: [{
-    id: 5,
-    name: 'Мъниста'
-  }, {
-    id: 4,
-    name: 'Панделка'
-  }],
-  size: 'стандартен',
-  picture: `assets/images/diadema.jpg`,
-  category: {
-    id: 3,
-    name: 'Аксесоари'
-  },
-  daysToMake: 3
-}, {
-  id: 6,
-  title: 'Ръчно изработена картонена торта',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
-  'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  price: {
-    amount: 20,
-    currency: 'BGN'
-  },
-  rating: 4.3,
-  materials: [{
-    id: 1,
-    name: 'Картон'
-  }, {
-    id: 3,
-    name: 'Стикери'
-  }, {
-    id: 4,
-    name: 'Панделка'
-  }],
-  size: '40см x 40см x 20см',
-  picture: `assets/images/torta2.jpg`,
-  category: {
-    id: 2,
-    name: 'Торти'
-  },
-  daysToMake: 5
-}, {
-  id: 7,
-  title: 'Огърлица със сърце',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
-  'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  price: {
-    amount: 7,
-    currency: 'BGN'
-  },
-  rating: 4.8,
-  materials: [{
-    id: 6,
-    name: 'Въже'
-  }, {
-    id: 7,
-    name: 'Висулка'
-  }],
-  size: 'стандартен',
-  picture: `assets/images/gerdan.jpg`,
-  category: {
-    id: 3,
-    name: 'Аксесоари'
-  },
-  daysToMake: 2
-}, {
-  id: 8,
-  title: 'Гривна със сърце',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
-  'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  price: {
-    amount: 7,
-    currency: 'BGN'
-  },
-  rating: 4.8,
-  materials: [{
-    id: 5,
-    name: 'Мъниста'
-  }, {
-    id: 7,
-    name: 'Висулка'
-  }],
-  size: 'стандартен',
-  picture: `assets/images/grivna.jpg`,
-  category: {
-    id: 3,
-    name: 'Аксесоари'
-  },
-  daysToMake: 2
-}, {
-  id: 9,
-  title: 'Обици',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
-  'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  price: {
-    amount: 8,
-    currency: 'BGN'
-  },
-  rating: 5,
-  materials: [{
-    id: 6,
-    name: 'Въже'
-  }, {
-    id: 7,
-    name: 'Висулка'
-  }],
-  size: 'стандартен',
-  picture: `assets/images/obici.jpg`,
-  category: {
-    id: 3,
-    name: 'Аксесоари'
-  },
-  daysToMake: 3
-}];
+  _parseResponse(response){
+    response.product = mappers.objToArray(response.product);
+    response.product = response.product.map(p => {
+      p.categories = mappers.objToArray(p.categories);
+      p.materials = mappers.objToArray(p.materials);
+      p.price = mappers.amountToPrice(p.price);
+      p.rating = this.reviewsRepository.getRatingForProduct(p.id);
+      return p;
+    });
+
+    return response.product;
+  }
+}
